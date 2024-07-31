bdd78894088192db959d5685af63eb60ac528ae2
Dheenadhayalan211203 <dheenashobha2k3@gmail.com>
2024-07-31 16:13:16 +0530

    modules deleted

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Landingpage from './Components/Landingpage';
  
 import Body from './Components/Body';
 import Startup from './Components/Startup';
+import Chatbox from './Components/Chatbox';
+import Signup from './Components/Signup';
  
 
 function App() {
@@ -13,6 +15,9 @@ function App() {
         <Route path="/" element={<Landingpage />}> </Route>
         <Route path='/body' element={<Body/>}></Route>
         <Route path='/auth' element={<Startup/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/chatbox' element={<Chatbox/>}></Route>
+        <Route path='/signin' element={<Signup/>}></Route>
       </Routes>
     </Router>
   );
--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -1,5 +1,11 @@
 import "./Landingpage.css"
+import { Navigate, useNavigate } from "react-router-dom"
 const Landingpage=()=>{
+    const navigate=useNavigate();
+    const handlestart=()=>
+    {
+        navigate('/auth')
+    }
     return(
         <>
            <section className="body">
@@ -16,7 +22,7 @@ const Landingpage=()=>{
                             <div><img src="logo.png" alt="" /></div>
                              
                              <h3>Start Chatting To Your Friends </h3>
-                             <div className="getbut">
+                             <div className="getbut" onClick={handlestart }>
                                 <h4>Get Started</h4>
                              </div>
                            </div>
